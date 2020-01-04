3c179e3bf70bc2ae4c2f7db1d5a3a36673750072
Romeo Soria <romeosoriajr@gmail.com>
2020-01-03 23:27:31 -0800

    Utilizing gridsome-plugin-pug for templates

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,18 +1,15 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'supro.me',
-  plugins: [],
-  port: 3030,
-  chainWebpack: config => {
-    config.module
-      .rule('pug')
-      .test(/\.pug$/)
-      .use('pug-plain-loader')
-      .loader('pug-plain-loader')
-  }
-}
+  siteName: "supro.me",
+  plugins: [
+    {
+      use: "gridsome-plugin-pug"
+    }
+  ],
+  port: 3030
+};
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,27 +1,12 @@
-<template>
-  <Layout>
-
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
-
-  </Layout>
+<template lang="pug">
+Layout
+  h1 Sup? I'm Rome.
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello, world!'
+    title: 'supro.me'
   }
 }
 </script>

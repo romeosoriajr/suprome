e2012665fdb41fbb51431c0de1d8e69c9882a3eb
Romeo Soria <romeosoriajr@gmail.com>
2020-06-07 21:09:40 -0700

    personal card formatting

---
+++ src/components/PersonalCard.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+.card.card-personal
+  .card-content
+    .media
+      .media-left
+        figure.image.is-128x128
+          img.is-rounded(id="profile-image" src="https://bulma.io/images/placeholders/256x256.png")
+      .media-content
+        p.title.is-4.has-text-light Romeo Soria Jr.
+        p.subtitle.is-6.has-text-light Web Developer, Full Stack JavaScript
+        p.subtitle.is-6.has-text-light hey@rome.af
+</template>
+<style>
+@media only screen and (min-width: 769px) {
+  .reverse-personal-order {
+    display: flex;
+    flex-direction: row-reverse;
+  }
+}
+
+.card-personal {
+  background-color: transparent;
+  box-shadow: 0 0 0 0;
+}
+</style>
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,39 +1,29 @@
 <template lang="pug">
 Layout
   section.section
-    .container.columns.is-desktop.is-vcentered
-      .column.is-offset-1
-        figure.image.is-256x256
-          img.is-rounded(id="profile-image" src="https://bulma.io/images/placeholders/256x256.png")
-      .column.is-offset-1
-        h1.title.has-text-light Sup? I'm Rome.
-        h2.subtitle.has-text-light doing things in js
-        div
-          p.has-text-light icon icon icon
+    .personalcontainer
+      PersonalCard
     img(id="landing-image" src="/images/bg_placeholder.jpg")
 </template>
 
 <script>
 import Layout from '@/layouts/Default'
+import PersonalCard from '@/components/PersonalCard'
 export default {
   metaInfo: {
     title: 'supro.me'
   },
-  components: { Layout }
+  components: { Layout, PersonalCard }
 }
 </script>
 <style>
-.container {
-  display: flex;
-  position: absolute;
-  left: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  z-index: 2;
+.personalcontainer {
+  width: 500px;
+  margin: auto;
 }
 #landing-image{
   position: absolute;
+  z-index: -2;
   left: 0;
   top: 0;
   width: 100%;
